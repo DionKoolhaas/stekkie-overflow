8af19bed85664edd9498a839cab83c8bb4e4ec76
Dion Koolhaas <dionkoolhaas@gmail.com>
2024-03-03 18:32:29 +0100

    hal browser werkend. spring security staat weer even uit

--- Readme.md
+++ Readme.md
@@ -2,3 +2,7 @@
 Run lokaal met het profiel "test".
 
 Enable annotation processing voor lombok.
+
+links:
+- hal browser http://localhost:8080/data-rest-api/
+- h2 console http://localhost:8080/h2-console , verbind met (jdbc url=jdbc:h2:mem:mydatabase, user name=test, password=test)
--- src/main/resources/testdata.sql
+++ src/main/resources/testdata.sql
@@ -2,8 +2,8 @@
 --  zorg dat target/test-classes wordt toegevoegd aan classpath indien spring boot opstart
 --  zodat de testdata niet mee komt in de uiteindelijke application package
 
-INSERT INTO PLANT (id, name, description) VALUES (1, 'Monstera', 'Ook wel de gatenplant genoemd');
+INSERT INTO PLANT (name, description) VALUES ('Monstera', 'Ook wel de gatenplant genoemd');
 
-INSERT INTO QUESTION(id, title, question_text, plant_id, user_id) VALUES (1, 'Wat te doen tegen bladluis?', 'Mijn monstera zit helemaal onder de bladluis. Ik heb middeltjes geprobeerd die ik bij de plantenshop had gekocht maar mocht niet baten. Heeft iemand een idee?',1, 'testgebruiker');
+INSERT INTO QUESTION(title, question_text, plant_id, user_id) VALUES ('Wat te doen tegen bladluis?', 'Mijn monstera zit helemaal onder de bladluis. Ik heb middeltjes geprobeerd die ik bij de plantenshop had gekocht maar mocht niet baten. Heeft iemand een idee?',1, 'testgebruiker');
 
-INSERT INTO ANSWER(id, answer_text, question_id, user_id) VALUES (1, 'Ik heb een mengsel van water, afwasmiddel en spiritus gebruikt. Werkt perfect!', 1, 'testgebruiker2' )
+INSERT INTO ANSWER(answer_text, question_id, user_id) VALUES ('Ik heb een mengsel van water, afwasmiddel en spiritus gebruikt. Werkt perfect!', 1, 'testgebruiker2' )
